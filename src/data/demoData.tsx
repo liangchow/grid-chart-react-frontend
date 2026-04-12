3baaafffb7bf95c188a5c7de96729ad55c56426b
liangchow <liangchern@gmail.com>
2026-04-11 22:52:51 -0700

    update types

---
+++ src/data/demoData.tsx
@@ -0,0 +1,12 @@
+export const demoData = [
+  { pressure: 0, void_ratio: 0.258 },
+  { pressure: 0.4, void_ratio: 0.254 },
+  { pressure: 0.8, void_ratio: 0.251 },
+  { pressure: 1.6, void_ratio: 0.239 },
+  { pressure: 3.2, void_ratio: 0.207 },
+  { pressure: 6.4, void_ratio: 0.182 },
+  { pressure: 12.8, void_ratio: 0.157 },
+  { pressure: 3.2, void_ratio: 0.17 },
+  { pressure: 0.8, void_ratio: 0.176 },
+]
+
